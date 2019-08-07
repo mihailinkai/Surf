0e3db53ef0ca2673e53c071c4fbb331a6b6d6e1d
mihailinkai <misha.izgareva@gmail.com>
2019-08-07 22:05:47 +0300

    add slider carousel

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,25 @@
-console.log('Hello Surfing!');
+$(document).ready(function(){
+
+var headerSlider =  $('.owl-carousel');
+headerSlider.owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    smartSpeed: 1000,
+
+});
+
+    $('#headerSliderLeft').click(function() {
+        console.log('Left');
+       headerSlider.trigger('prev.owl.carousel');
+    })
+  
+    $('#headerSliderRight').click(function() {
+        console.log('Right');
+        headerSlider.trigger('next.owl.carousel');
+    })
+
+
+
+
+  });
